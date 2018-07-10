cae202cf08658ad374e24dd55f6a2821d9b025c5
Chase McCarthy <chase@code0100fun.com>
2018-07-10 18:40:10 -0500

    Add .editorconfig

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const secrets = Yaml.safeLoad(fs.readFileSync('secrets.yml', 'utf8'));
 const bot = new Discord.Client();
 
 bot.on('ready', () => {
-    console.log(`Logged in as ${bot.user.tag}!`);
+  console.log(`Logged in as ${bot.user.tag}!`);
 });
-  
+
 bot.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
-    }
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
 bot.login(secrets['bot_token']);
