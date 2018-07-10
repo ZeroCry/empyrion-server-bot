1d12c46392f3355e748121abf162a9862c90a6a2
Chase McCarthy <chase@code0100fun.com>
2018-07-10 14:28:58 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const Yaml = require('js-yaml');
+const Discord = require('discord.js');
+
+const secrets = Yaml.safeLoad(fs.readFileSync('secrets.yml', 'utf8'));
+
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log(`Logged in as ${bot.user.tag}!`);
+});
+  
+bot.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
+
+bot.login(secrets['bot_token']);
